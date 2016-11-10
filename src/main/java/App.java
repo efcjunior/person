802bb236d6f0f8fc5dc23529751710bb4f733314
efcjunior <everson.c.junior@gmail.com>
2016-11-10 00:51:42 -0200

    ets

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
-        port(80);
+        port(8080);
         get("/hello", (req,res) -> "Hello World");
     }
 }
